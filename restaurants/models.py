4d19ad129aef7dc7c6bac2d34afb24786405533d
PC <fdhfdh>
2020-02-18 09:30:28 +0200

    adding task 10

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class Restaurant(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE,null=True)
     name = models.CharField(max_length=120)
     description = models.TextField()
     opening_time = models.TimeField()
@@ -9,3 +12,9 @@ class Restaurant(models.Model):
 
     def __str__(self):
     	return self.name
+
+class Item (models.Model):
+	name = models.CharField(max_length=50)
+	description = models.TextField()
+	price = models.DecimalField (max_digits=5, decimal_places=2)
+	restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
